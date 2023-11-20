e3c9434fc740ce7b60b3bf4914e86eb8c7b23b4b
Boris SdK <borferkic@gmail.com>
2023-11-20 00:08:23 -0500

    Puliendo el scrip despues del testeo

--- Scripts/WhatsBomber-SinTexto.js
+++ Scripts/WhatsBomber-SinTexto.js
@@ -1,28 +1,27 @@
-async function StartBombing(scriptText){
-	const lineas = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+{
+	const dialogos = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
 	main = document.querySelector("#main"),
 	textarea = main.querySelector(`div[contenteditable="true"]`)
-	
-	if(!textarea) throw new Error("La conversacion no esta abierta")
-	
-	for(const line of lineas){
+	if(!textarea) throw new Error("La conversacion no esta abierta") //En caso de error
+	//Funcion de escritura
+	for(const line of dialogos)
+	{
 		console.log(line)
 		textarea.focus();
 		document.execCommand('insertText', false, line);
 		textarea.dispatchEvent(new Event('change', {bubbles: true}));
-	
-		setTimeout(() => {
+		setTimeout(() => 
+		{
 			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
 		}, 100);
-		
-		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+		if(dialogos.indexOf(line) !== dialogos.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
 	}
-	
-	return lines.length;
+	return dialogos.length;
 }
 //Reemplazar el texto de la linea 26 por el dialogo o mensajes que desees enviar//
 enviarScript(`
 
 PONER ACA EL SCRIPT DE LA PELICULA
 
-`).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
+`).then(e => console.log(`Bombardeo finalizado, ${e} mensajens enviados`)).catch(console.error)
+//Fin del script
